c1bf829579af5ca20cf4e70b3b8424f2e839ff6d
JoseA <joseantanaper@gmail.com>
2024-01-24 14:10:56 +0100

    Base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://joseantanaper.github.io/',
-  base: '/astro-react-bootstrap-template'
+  base: '/astro-bootstrap-template'
 })
 
