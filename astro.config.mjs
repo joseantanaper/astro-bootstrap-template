20e620f2273b207f58ef21616f6b0099aa8b04e3
JoseA <joseantanaper@gmail.com>
2024-02-21 14:41:50 +0100

    Netlify test 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ export default defineConfig({
   // site: 'https://joseantanaper.github.io/',
   // site: 'http://glistening-dango-c88f0d.netlify.app',
   // base: '/astro-bootstrap-template'
-  base: '/'
+  // base: '/'
 })
 
