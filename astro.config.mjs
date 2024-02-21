90875a3a7022d0176abaa85b1c550627f3b50c0e
JoseA <joseantanaper@gmail.com>
2024-02-21 14:04:39 +0100

    _astro to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    // Default _astro is not working on github pages.
+    assets: 'astro/'
+  },
   site: 'https://joseantanaper.github.io/',
   base: '/astro-bootstrap-template'
 })
