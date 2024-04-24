f1601d853b97bb5482c255f16f2816fe82543913
JoseA <joseantanaper@gmail.com>
2024-04-24 19:19:00 +0200

    site config param

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  site:
+    import.meta.env.PROD === false
+      ? 'http://localhost:4321'
+      : 'https://joseantanaper.github.io/astro-bootstrap-template/'
   // site: 'https://joseantanaper.github.io/',
   // site: 'http://glistening-dango-c88f0d.netlify.app',
   // base: '/astro-bootstrap-template'
